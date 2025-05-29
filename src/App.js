efd3f623f36a686c45fe2814b9d5e585e67ee68a
Ayrton Pireda <piredaayrton@gmail.com>
2025-05-29 16:56:04 -0300

    random-color generator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color';
 import data from "./components/accordion/data"
 
 
 function App() {
   return (
     <div className="App">
-      <Accordion data={data}/>
+      {/* <Accordion data={data}/> */}
+      <RandomColor />
     </div>
   );
 }
