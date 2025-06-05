c373e4760f6318c2196831841b5a035d19d34d9a
Ayrton Pireda <piredaayrton@gmail.com>
2025-06-05 18:15:43 -0300

    image slider component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import data from "./components/accordion/data";
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
-import data from "./components/accordion/data";
+import ImageSlider from './components/image-slider'
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       {/* <Accordion data={data}/> */}
       {/* <RandomColor /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={"10"} page={"1"}/>
     </div>
   );
 }
