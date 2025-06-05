e20d52f297fe2b0977db1281b57aeda6ba7ed1da
Ayrton Pireda <piredaayrton@gmail.com>
2025-06-05 16:45:20 -0300

    star rating component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
-import data from "./components/accordion/data"
+import StarRating from './components/star-rating';
+import data from "./components/accordion/data";
 
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion data={data}/> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </div>
   );
 }
