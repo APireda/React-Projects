7931e1e50b658a8142ef5e41b22e16db4df97b00
Ayrton Pireda <piredaayrton@gmail.com>
2025-06-12 19:10:43 -0300

    tree-view component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import data from "./components/accordion/data";
+import menus from './components/tree-view/data';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider'
 import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
       {/* <RandomColor /> */}
       {/* <StarRating /> */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={"10"} page={"1"}/> */}
-      <LoadMoreData url={'https://dummyjson.com/products?limit=20'} />
+      {/* <LoadMoreData url={'https://dummyjson.com/products?limit=20'} /> */}
+      <TreeView menus={menus} />
     </div>
   );
 }
